83d7f0ba7498de29c0c4158f9a5f6de3c94e48c4
pyprism <git.pyprism@gmail.com>
2014-04-06 17:43:38 +0600

    added new package for server side

---
+++ server/model/hiren.js
@@ -0,0 +1,19 @@
+/**
+ * Created by prism on 4/6/14.
+ */
+
+var logentries = require('node-logentries'),
+    mongoose = require('mongoose') ,
+    config = require('../config.js');
+
+var log = logentries.logger({
+    token: config.token
+});
+
+console.log(config.token);
+
+// level specific methods like 'info', 'debug', etc.
+log.info("I'm a Lumberjack and I'm OK")
+
+// generic log method, also accepts JSON entries
+log.log("debug", {sleep:"all night", work:"all day"})
